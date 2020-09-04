6e1c8a14dc274e6acb2ac64925fa49a71b2bc2ed
X-Skywalker-MY <66560178+X-Skywalker-MY@users.noreply.github.com>
2020-09-04 09:01:52 +0800

    CUPMAT_loT
    ID：19242	名称：物联网水杯垫-HFUT	APIKEY：712c5cad8

--- CUPMATv1.0/CUPMATv1.0.ino
+++ CUPMATv1.0/CUPMATv1.0.ino
@@ -3,15 +3,15 @@
     项目：校级2020大创_智能物联网水杯垫
     版 本：V1.0
     说 明：
-    20200902 吴**  为什么布尔值不用bool，其实都一样，去看看C标准就知道bool其实就是int，
+    20200902 吴韦举  为什么布尔值不用bool，其实都一样，去看看C标准就知道bool其实就是int，
                     arduino执行的C语言标准比keil51新多了，很多写法和keil不一样
                     维护此项目请做好测试以及写好修改说明
-                    下一步工作将使用三线程，我将继续修复bug,以及重写导入的库文件所用到的函数以及简化代码，向**将为此项目添加esp8266串口数据传输的功能，成**继续完成提醒部分的程序
+                    下一步工作使用三线程，我将继续修复bug,以及重写导入的库文件所用到的函数以及简化代码，向勉益将为此项目添加esp8266串口数据传输的功能，成帅继续完成提醒部分的程序
                        
-    修改记录:20200830以前  成**  创建
-            20200831 吴**  格式优化；代码整合；细节优化；
+    修改记录:20200830以前  成帅  创建
+            20200831 吴韦举  格式优化；代码整合；细节优化；
                             试图编写scale_antishake()函数，失败，暂时弃用了
-            20200902 吴**  添加LCD1602_initialize(),LCD1602_welcome()两个开机显示函数
+            20200902 吴韦举  添加LCD1602_initialize(),LCD1602_welcome()两个开机显示函数
                             用remind1_s()暂时替代remind1()，前者显示秒数，因为在开发者调试过程中，分钟时间过长，不方便，开发者使用后者调试更方便
                             主程序方面，完成了饮水过程的准确识别以及饮水过程中的时间差与重量差的读取，提醒部分，计划书写得并不一定合理，故暂只象征性编写了小部分程序，此部分以后编程难度很小
                             经测试，主体功能似乎实现了！版本1.0  bug应该会很多，以后再调
@@ -26,6 +26,7 @@
 #include "HX711.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h> //这两个库是与LCD1602有关的库文件
+#include <aJSON.h>
 //此板块以后将进行整理，将库文件里面我们需要的代码进行重写，现在暂时使用他人的库函数
 
 /**********************************************************
@@ -50,6 +51,16 @@ DHTesp dht;
 HX711 scale;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 //I2C LCD显示屏初始化,本来是打算放在函数里面的，但是没办法，放在函数里面会出现一些问题，故没有封在函数里面。
+/**********************************************************
+                      贝壳物联接口定义
+***********************************************************/
+String DEVICEID = "19242";      //本设备在贝壳物联上的设备ID
+String APIKEY = "712c5cad8";    //设备密码
+String WORKID = "17229";        //接口“是否在使用中”ID
+String NO_DRINK_SID = "17230";  //接口”未饮水时间（s）“ID
+String NO_DRINK_MINID = "17232";//接口”未饮水时间（min）“ID
+String DRINK_TIMEID = "17231 "; //接口”饮水次数“ID
+String RECORDDRINKID = "13949 "; //接口”饮水量“ID
 
 /**********************************************************
                       全局外部控制变量定义
@@ -91,6 +102,17 @@ int layflag = 0;                  //布尔值，判断水杯在不在水杯垫�
 int lastlayflag = 0;              //布尔值，判断上一次loop循环水杯在不在水杯垫上，默认为0
 int drinking_leaving = 0;         //布尔值，判断一次饮水过程中是否处于拿起水杯喝水（水杯离开水杯垫）的状态，默认为0
 unsigned long temp = 0;           //临时变量，随意调用，用完归零。
+
+unsigned long lastCheckInTime = 0;              //记录上次报到的时间
+unsigned long lastUpdateTime = 0;               //记录上次上传数据的时间
+const unsigned long postingInterval = 40000;    //若未报到成功，每隔40s向服务器报到一次
+const unsigned long updatedInterval = 5000;     //数据上次间隔5s
+String inputString = "";                        //串口读取到的内容
+boolean stringComplete = false;                 //串口是否读取完毕
+boolean CONNECT = true;                         //连接状态
+boolean isCheckIn = false;                      //是否连接状态
+char* parseJson(char *jsonString);              //定义aJson字符串
+
 
 double scale_antishake_weight[5];  //用于压力传感器的防抖的临时数组
 double scale_antishake_gap = 0;
@@ -304,7 +326,50 @@ void loop()//目前一次loop循环运行时间不超过2s,可以接受，最好
   /*old_time = 0;
     while (millis() < old_time + 1500) {}
     old_time = millis();*/
+
+  /**********************************************************
+                         贝壳物联代码
+  ***********************************************************/
+  if(millis() - lastCheckInTime > postingInterval||lastCheckInTime == 0)
+  {
+    CheckIn();
+  }
+  if(millis() - lastUpdateTime > updatedInterval)
+  {
+    updatel(DEVICEID,"17229",(float)layflag );                         //上传数据”是否在使用中“
+    updatel(DEVICEID,"17232",(float)no_drink_time );                   //上传数据”未喝水时间（min)"
+    updatel(DEVICEID,"17230",(float)no_drink_time_s );                 //上传数据"未喝水时间(s)"
+    updatel(DEVICEID,"17231",(float)drink_times );                     //上传数据"饮水次数“
+    updatel(DEVICEID,"13949",(float)recordwater[drink_times] );        //上传数据”饮水量“
+  }
+  serialEvent();                              //调用serialEvent()函数获取网站传输的指令
+  if(stringComplete)
+  {
+    inputString.trim();
+    //Serial. println( inputString);
+    if(inputString == "CLOSED")               // 如果接收到网站的指令为CLOSED,则停止连接
+    {
+      Serial. println("connect closed!");
+      CONNECT = false;
+      isCheckIn = false;
+    }
+    else                                     //否则，处理接收到的命令
+    {
+      int len = inputString.length() + 1;
+      if (inputString.startsWith("{") && inputString.endsWith("}"))
+      {
+        char jsonString[len];
+        inputString.toCharArray(jsonString, len);
+        aJsonObject * msg = aJson.parse(jsonString);
+        processMessage( msg) ;              //处理接收到的JSON数据
+        aJson.deleteItem(msg);
+      }
+   }
+    inputString = "";
+    stringComplete = false;
+ }
 }
+
 
 /**********************************************************
                         自定义函数
@@ -318,7 +383,7 @@ void loop()//目前一次loop循环运行时间不超过2s,可以接受，最好
     输入参数：
     输出参数：
     返回值：无
-    说明：需要初始化LCD1602，，现有bug，执行一次后原设定的常亮3s不再出现，原因不详，估计与millis函数有关,     20200902 吴**  已修复
+    说明：需要初始化LCD1602，，现有bug，执行一次后原设定的常亮3s不再出现，原因不详，估计与millis函数有关,     20200902 吴韦举  已修复
          20200902发现新bug，当no_drink_time为一位数时，感叹号显示有点问题，小bug,赶工暂未修复
 ***********************************************************/
 void remind1()
@@ -435,7 +500,7 @@ void remind1()
     输入参数：
     输出参数：
     返回值：无
-    说明：需要初始化LCD1602，，现有bug，执行一次后原设定的常亮3s不再出现，原因不详，估计与millis函数有关,     20200902 吴** 已修复
+    说明：需要初始化LCD1602，，现有bug，执行一次后原设定的常亮3s不再出现，原因不详，估计与millis函数有关,     20200902 吴韦举 已修复
          20200902发现新bug，当no_drink_time为一位数时，感叹号显示有点问题，小bug,赶工暂未修复
          仅用于开发者测试！！！
 ***********************************************************/
@@ -760,7 +825,7 @@ void LCD1602_usual_balance()
     输入参数：
     输出参数：
     返回值：无
-    说明：20200831 吴**编写，
+    说明：20200831 吴韦举编写，
     此函数工作不正常，请勿使用
 ***********************************************************/
 void scale_antishake()
@@ -796,4 +861,100 @@ void scale_antishake()
   Serial.print("\t\n");
   Serial.print(scale_antishake_gap);
   scale_antishake_gap = 0;
+}
+/***********************************************************
+    函数名称：CheckIn()
+    函数功能：连接失败后重新连接
+    调用函数：
+    输入参数：
+    输出参数：
+    返回值：无
+    说明：如果连接失败，则通过AT指令重新连接，否则输入设备ID和密码进行连接
+***********************************************************/
+void CheckIn()
+{
+  if(!CONNECT)                  //如果连接失败，则通过AT指令重新连接
+  {
+    Serial.print("++ +");
+    delay(500);
+    Serial.print("\r\n");
+    delay(1000);
+    Serial.print("AT+RST\r\n");
+    delay(600); 
+    CONNECT =  true;
+    lastCheckInTime = millis();   
+  }
+  else                           //否则输入设备ID和密码进行连接
+  {
+    Serial.print("{\"M\":\"checkin\",\"ID\":\"");
+    Serial.print(DEVICEID);
+    Serial.print("\",\"K\":\"");
+    Serial.print(APIKEY);
+    Serial.print("\"}\r\n"); 
+    lastCheckInTime = millis();
+  }  
+}
+/***********************************************************
+    函数名称：updatel()
+    函数功能：上传数据至贝壳物联
+    调用函数：
+    输入参数：
+    输出参数：
+    返回值：无
+***********************************************************/
+void updatel(String did, String inputid, float value)CUPMAT
+{
+    Serial.print("{\"M\":\"update\",\"ID\":\"");
+    Serial.print(did);
+    Serial.print("\",\"V\":{\"");
+    Serial.print(inputid);
+    Serial.print("\"}}");
+    lastCheckInTime = millis();   
+    lastCheckInTime = millis();       
+}
+/***********************************************************
+    函数名称：serialEvent()
+    函数功能：获取网络传输的指令，读取网站的命令
+    调用函数：
+    输入参数：
+    输出参数：
+    返回值：无
+***********************************************************/
+void serialEvent()
+{
+  while(Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if(inChar == '\n')
+    {
+      stringComplete = true; 
+    }
+  }  
+}
+/***********************************************************
+    函数名称：processMessage()
+    函数功能：处理接受来自网站的命令
+    调用函数：
+    输入参数：
+    输出参数：
+    返回值：无
+***********************************************************/
+void processMessage(aJsonObject*msq)
+{
+  aJsonObject*method = aJson.getObjectItem(msq,"M");
+  aJsonObject*content = aJson.getObjectItem(msq,"C");
+  aJsonObject*client_id = aJson.getObjectItem(msq,"ID");
+  //char*st = aJson.print(msg);
+  if (!method) 
+  {
+    return;
+  }
+  //Serial. println(st);
+  //free(st);
+    String M = method -> valuestring;
+  if (M == "checkinok" )
+  {
+    isCheckIn = true;
+  }
 }
